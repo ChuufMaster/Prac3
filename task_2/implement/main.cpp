63fd23b5596cced8c405972efaff11c0869e26df
ChuufMaster <ivanhorak20@gmail.com>
2023-09-26 10:52:09 +0200

    update

--- task_2/implement/main.cpp
+++ task_2/implement/main.cpp
@@ -11,6 +11,15 @@ int main(int argc, const char** argv) {
         if(param == "add"){
             getline(cin, param);
             menu->addTile(param);
+        }else if(param == "research"){
+            getline(cin, param);
+            menu->researchTrap(param[0]);
+        }else if(param == "map"){
+            menu->printMap();
+        }else if(param == "remove research"){
+            menu->removeTrapFromQueue(param[0]);
+        }else if(param == "horde"){
+            menu->viewHorde();
         }
     }
     return 0;
