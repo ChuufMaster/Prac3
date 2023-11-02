7d942bfd7e1f4e020607b20ba28fd5d7e8263a16
ChuufMaster <82512988+ChuufMaster@users.noreply.github.com>
2023-11-02 18:44:28 +0200

    update

---
+++ input.sh
@@ -0,0 +1,29 @@
+echo "MyContract"
+
+# Provide number of parties
+#echo "Number of Parties:"
+echo "3"
+
+# Add conditions
+echo "Y"  # Add condition Y/N
+echo "Condition 1"
+echo "y"  # Continue adding conditions
+echo "Condition 2"
+echo "n"  # Stop adding conditions
+
+# Remove conditions
+echo "Y"  # Remove Condition? Y/N
+echo "Condition 1"
+echo "n"  # Stop removing conditions
+
+# Accept contract
+echo "Y"
+echo "Y"
+echo "Y"
+
+# Complete contract
+echo "Y"
+echo "N"
+
+# Make another contract
+echo "N"
--- task_1/main.cpp
+++ task_1/main.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-int main(int argc, const char** argv) {
+int main() {
 
     //cout << top << ═ << ╬ << ═ <<endl << __ << ╩;
     bool cont_contract = true;
@@ -16,14 +16,13 @@ int main(int argc, const char** argv) {
         cout << "Contract name:\t";
         string contract_name;
         //cin >> contract_name;
-        getline(cin, contract_name);
+        getline(cin >> ws, contract_name);
+        cout << contract_name << endl;
 
         int num_parties;
         cout << "Number of Parties:\t";
-        //cin >> num_parties;
-        string input;
-        getline(cin, input);
-        num_parties = stoi(input);
+        cin >> num_parties;
+        cout << num_parties << endl;
 
         Subscriber* sub = new Subscriber();
         Contract* loop_contract = new Contract(num_parties, contract_name, sub);
@@ -34,8 +33,9 @@ int main(int argc, const char** argv) {
 
         add_another:
             cout << "Add condition Y/N:\t";
-            //cin >> add_cond;
-            getline(cin, add_cond);
+            cin >> add_cond;
+            //getline(cin, add_cond);
+            cout << add_cond << endl;
 
             //while (add_cond != "Y" || add_cond != "y" || add_cond != "N" || add_cond != "n") {
             //    cout << "Add condition Y/N:\t";
@@ -46,41 +46,51 @@ int main(int argc, const char** argv) {
                 string condition;
                 cout << "Condition?\t";
                 //cin >> condition;
-                getline(cin, condition);
+                getline(cin >> ws, condition);
                 loop_contract->addCondition(condition);
+                cout << condition << endl;
 
                 cout << loop_contract->toString();
                 cout << "Do you wish to continue adding conditions (y or n)?\t";
-                //cin >> add_cond;
-                getline(cin, add_cond);
+                cin >> add_cond;
+                //getline(cin, add_cond);
+                cout << add_cond << endl;
 
             }
 
             if (add_cond == "N" || add_cond == "n") {
                 string remove;
                 cout << "Remove Condition? Y/N:\t";
-                getline(cin, remove);
+                //getline(cin >> ws, remove);
+                cin >> remove;
+                cout << remove << endl;
                 while (remove == "Y" || remove == "y") {
                     cout << "Which condition do you want removed?" << endl;
                     string condition_to_remove;
-                    getline(cin, condition_to_remove);
+                    //cin >> condition_to_remove;
+                    getline(cin >> ws, condition_to_remove);
+                    cout << condition_to_remove << endl;
                     loop_contract->removeCondition(condition_to_remove);
                     cout << loop_contract->toString();
                     cout << "Would you like to remove another? Y/N:\t";
-                    getline(cin, remove);
+                    cin >> remove;
+                    //getline(cin, remove);
+                    cout << remove << endl;
                 }
 
                 string accept;
                 cout << "Accept Y/N:\t";
-                //cin >> accept;
-                getline(cin, accept);
+                cin >> accept;
+                //getline(cin, accept);
+                cout << accept << endl;
                 int count = 0;
                 while (accept == "Y" || accept == "y") {
                     loop_contract->accept();
                     cout << endl << loop_contract->toString() << endl;
                     cout << "Accept Y/N:\t";
-                    //cin >> accept;
-                    getline(cin, accept);
+                    cin >> accept;
+                    //getline(cin, accept);
+                    cout << accept << endl;
                     count++;
                     if (count == num_parties)
                         break;
@@ -91,20 +101,22 @@ int main(int argc, const char** argv) {
             }
             string complete;
             cout << "Complete contract? Y/N:\t";
-            //cin >> complete;
-            getline(cin, complete);
+            cin >> complete;
+            //getline(cin, complete);
+            cout << complete << endl;
             int complete_count = 0;
             while (complete == "Y" || complete == "y") {
                 loop_contract->complete();
                 cout << endl << loop_contract->toString() << endl;
                 cout << "Complete contract? Y/N:\t";
-                //cin >> complete;
-                getline(cin, complete);
+                cin >> complete;
+                //getline(cin, complete);
+                cout << complete << endl;
 
                 while (complete == "N" || complete == "n") {
                     cout << "Complete contract? Y/N:\t";
-                    //cin >> complete;
-                    getline(cin, complete);
+                    cin >> complete;
+                    //getline(cin, complete);
                 }
                 complete_count++;
                 if (complete_count == num_parties) {
@@ -116,7 +128,9 @@ int main(int argc, const char** argv) {
             }
             string make_another;
             cout << "Would you like to make another contract? Y/N:\t";
-            getline(cin, make_another);
+            cin >> make_another;
+            //getline(cin, make_another);
+            cout << make_another << endl;
             if (make_another == "Y" || make_another == "y") {
                 delete loop_contract;
                 cont_loop = true;
